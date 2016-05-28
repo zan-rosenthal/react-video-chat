523c0448a111d29b03602ca9b03141c7a77b8faa
zan-rosenthal <Zan@Zans-MacBook-Pro.local>
2016-05-28 17:49:23 -0400

    Set up express to use handlebars and render App component from server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
+import path from 'path';
 import express from 'express';
+import handlebars from 'express-handlebars';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from './generated/app'
 
 const app = express();
+const port = process.env.PORT || 3000;
 
+//Views
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+//Static assets
+app.use(express.static(path.resolve(__dirname, '../dist')))
+
+//Routes
 app.get('/', (request, response) => {
-  response.status(200).send("Hiya")
+  response.render('app', {
+    app: ReactDOMServer.renderToString(<App />)
+  });
 });
 
-app.listen(3000, ()=> console.log('Server running'));
+app.listen(port, ()=> console.log('Server running on port: ', port));
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,74 @@
 const path = require('path');
-const CLIENT_DIR = path.resolve(__dirname, 'client');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  context: CLIENT_DIR,
-  entry: './index.js',
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module:{
-    loaders:[
-      {
-        test: /\.js$/,
-        include: CLIENT_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      {
-        test: /\.less$/,
-        loader: 'style-loader!css-loader!less-loader'
-      }
-    ]
-  },
-  resolve:{
-    alias: {
-      components: path.resolve(CLIENT_DIR, 'components')
+const CLIENT_DIR = path.resolve(__dirname, 'client');
+const SERVER_DIR = path.resolve(__dirname, 'server/generated');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+
+const loaders = [
+  {
+    test: /\.js$/,
+    include: CLIENT_DIR,
+    loader: 'babel-loader',
+    query: {
+      presets: ['es2015', 'react']
     }
   },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+  {
+    test: /\.less$/,
+    loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
   }
-}
+];
+
+module.exports = [{
+    name:'client',
+    target: 'web',
+    context: CLIENT_DIR,
+    entry: './index.js',
+    output: {
+      path: DIST_DIR,
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module:{
+      loaders:loaders
+    },
+    resolve:{
+      alias: {
+        components: path.resolve(CLIENT_DIR, 'components')
+      }
+    },
+    plugins:[
+      new ExtractTextPlugin('bundle.css', {allChunks: true})
+    ],
+    devServer: {
+      historyApiFallback: true,
+      contentBase: './'
+    }
+  },
+  {
+    name: 'server',
+    target: 'node',
+    context: CLIENT_DIR,
+    entry:{
+      app: 'components/app/index.js'
+    },
+    output:{
+      path: SERVER_DIR,
+      filename: '[name].js',
+      libraryTarget: 'commonjs2'
+    },
+    externals: /^[a-z\-0-9]+$/,
+    module: {
+      loaders: loaders
+    },
+    resolve:{
+      alias:{
+        components: path.resolve(CLIENT_DIR, 'components')
+      }
+    },
+    plugins:[
+      new ExtractTextPlugin('[name].css')
+    ]
+  },
+];
